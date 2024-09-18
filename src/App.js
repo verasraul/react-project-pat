2e3b162c1b4df9526940ee6ac87aa131485783a5
Raul Veras <verasraul@gmail.com>
2024-09-18 17:36:52 -0400

    adding header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Header from "./components/Header";
 import SignInForm from "./components/SignInForm";
+import SignUpForm from "./components/SignUpForm";
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <SignInForm />
+      <SignUpForm />
     </div>
   );
 }
